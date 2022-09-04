b579858bb21cc9383ed4e8429f243a29d18c3bac
brualvess <oliveirabrunna2210@gmail.com>
2022-09-04 11:02:02 -0300

    fix

--- src/routes.ts
+++ src/routes.ts
@@ -24,9 +24,9 @@ const router = Router()
 // routes cards
 router.post('/card', joiValidation(schemaCard), keyApi, createCard)
 router.put('/activateCard', joiValidation(schemaActivateCard), activateCard)
-router.get('/balanceCard/:id', getBalanceTransaction)
-router.put('/blockCard', joiValidation(schemaBlocksCard), blockCard)
-router.put('/unblockCard', joiValidation(schemaBlocksCard), unblockCard)
+router.get('/balance/:id', getBalanceTransaction)
+router.put('/block', joiValidation(schemaBlocksCard), blockCard)
+router.put('/unblock', joiValidation(schemaBlocksCard), unblockCard)
 // routes recharges
 router.post('/recharges',joiValidation(schemaRecharges),keyApi, recharges)
 //routes payments
